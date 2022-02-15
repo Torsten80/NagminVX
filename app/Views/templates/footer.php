a2fef9a55ad4cf0a84f1ed5fbc174cadca263f61
Torsten80 <61577301+Torsten80@users.noreply.github.com>
2022-02-15 18:14:32 +0100

    Add files via upload

--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -1,5 +1,5 @@
 <p><table width=100% style="border: none;"><tr style="border: none;"><td style="border: none;">
-       <b><FONT SIZE="2"> NagminVX  </FONT></b><small>Version: 20211218  <em> </td><td style="border: none;"> <small> &copy;  Torsten Fährmann 2019-2021</em></small></td>
+       <b><FONT SIZE="2"> NagminVX  </FONT></b><small>Version: 20220215  <em> </td><td style="border: none;"> <small> &copy;  Torsten Fährmann 2019-2022</em></small></td>
   </tr></table>    
                
         </body>
